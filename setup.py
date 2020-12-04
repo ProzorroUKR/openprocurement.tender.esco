9e6a3a00d9ad43a81f3b3415bc023a14088980a2
smithumble <33865176+smithumble@users.noreply.github.com>
2020-12-04 17:38:50 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ entry_points = {
 
 here = os.path.abspath(os.path.dirname(__file__))
 
-with open(os.path.join(here, 'README.rst')) as f:
+with open(os.path.join(here, 'README.md')) as f:
     README = f.read()
 
 setup(name='openprocurement.tender.esco',
